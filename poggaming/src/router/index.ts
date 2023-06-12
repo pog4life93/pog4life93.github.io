739550e16015084f4a2ee67c757873ef086f57b6
Autumn <autumn.leigh.henderson@gmail.com>
2023-06-12 14:33:29 -0400

    Updated components and imports for current architecture in router

--- poggaming/src/router/index.ts
+++ poggaming/src/router/index.ts
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MainContainer from '../views/MainContainer.vue'
+import { env } from 'node:process'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: MainContainer
     },
     {
       path: '/about',
