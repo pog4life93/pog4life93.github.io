8a4a728e291044d9121e54c1e05ec2466dd09d8b
Autumn <autumn.leigh.henderson@gmail.com>
2023-06-12 14:33:45 -0400

    Added new boilerplate files

---
+++ poggaming/src/components/TopNav.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import M from 'materialize-css'
+
+initialize()
+
+function initialize() {
+  document.addEventListener('DOMContentLoaded', () => {
+		let elems = document.querySelectorAll('.sidenav');
+		let options = {edge: 'left'};
+		let instances = M.Sidenav.init(elems, options);
+
+		let dropdownTrigger = document.querySelectorAll('.dropdown-trigger');
+		let dropdownOptions = {coverTrigger: false, hover: true};
+		let dropdownInstance = M.Dropdown.init(dropdownTrigger, dropdownOptions);
+	});
+}
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ 'Hello world' }}</h1>
+
+  </div>
+</template>
+
+<style lang="less" scoped>
+
+</style>
---
+++ poggaming/src/views/MainContainer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TopNav from '@/components/TopNav.vue'
+</script>
+
+<template>
+  <main>
+    <TopNav />
+  </main>
+</template>
+
+<style lang="less" scoped></style>
