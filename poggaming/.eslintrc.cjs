15d17d77b38de099e33f03606c6b2769b5feff6a
Autumn <autumn.leigh.henderson@gmail.com>
2023-06-16 13:13:36 -0400

    Linting configurations

--- poggaming/.eslintrc.cjs
+++ poggaming/.eslintrc.cjs
@@ -1,15 +1,28 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        tabWidth: 2,
+        singleQuote: true,
+        printWidth: 100,
+        endOfLine: 'auto',
+        htmlWhiteSpaceSensitivity: 'ignore',
+        bracketSameLine: true,
+      },
+    ],
+  },
+};
