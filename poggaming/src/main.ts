df0befd99fc08144ec295daaaf00018230f943b2
Autumn <autumn.leigh.henderson@gmail.com>
2023-06-16 13:15:05 -0400

    Prettier config

--- poggaming/src/main.ts
+++ poggaming/src/main.ts
@@ -1,12 +1,12 @@
-import './assets/css/main.less'
-import 'materialize-css/dist/css/materialize.min.css'
+import './assets/css/main.less';
+import 'materialize-css/dist/css/materialize.min.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
