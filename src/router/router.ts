d4896ef3dccc858f5832406e06b8e0bb60ab5224
Autumn <autumn.leigh.henderson@gmail.com>
2023-07-09 22:15:22 -0400

    Third attempt at fixing routing

--- src/router/router.ts
+++ src/router/router.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
-  history: createWebHistory('/pog4life93.github.io/'),
+  history: createWebHistory(),
   routes: [
     // {
     //   path: '/',
